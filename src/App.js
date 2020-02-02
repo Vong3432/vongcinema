093a7d06ade1a9cb7276ca43d8ce0aabf5d98bd9
Vong3432 <vongnyuksoon2000@gmail.com>
2020-02-03 00:21:04 +0800

    test

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TopBar from './components/TopBar';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+// Pages
+import Home from './pages/Home'
+import Movie from './pages/Movie'
+import News from './pages/News'
+import Contact from './pages/Contact'
+import Detail from './pages/Detail';
+
+function App() {  
+
+  return (
+    <div className="App">
+      <Router>    
+        <TopBar />
+        <Switch>
+          <Route path="/vongcinema" exact component={Home} />
+          <Route path="/movie" exact component={Movie} />
+          <Route path="/news" exact component={News} />
+          <Route path="/contact" exact component={Contact} />
+          <Route path="/m/:movieid" exact component={Detail} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
